fd31fe94653a5fe4bf3b8899d4bdde365c3e0248
Jesuschock <jesuschock93@gmail.com>
2022-10-02 16:03:25 -0400

    commit-4:02PM -- controllers

---
+++ src/todos/todos.controllers.js
@@ -0,0 +1,63 @@
+// import & Using generator key -> UUID
+const uuid = require('uuid')
+
+
+// DATABASE USERS object like JSON
+const usersDB = [
+    {
+        id: 'c0f6a7c2-e462-47a5-b62b-901ad7c467fa',
+        first_name: 'Jesús',
+        last_name: 'Arechiderg',
+        email: 'jesuschock93@gmail.com',
+        password: 'ja1234',
+        birthday: '1993/12/01',
+        is_active: true,
+        url: `http://127.0.0.1:9000/users/c0f6a7c2-e462-47a5-b62b-901ad7c467fa`
+    }
+];
+
+
+/************* GET *************************/
+
+// Getting All DB users
+const getAllUsers = () =>{
+    return usersDB
+};
+
+// Getting All DB users
+const getUserById = id => {
+    const userById = usersDB.find(user => user.id === id)
+    return userById
+};
+
+/************* POST - CREATE ***************/
+const createNewUser = (first_name, last_name, email, password, birthday) => {
+
+    let uuidGenerate = uuid.v4()
+
+    const newUser = {
+        id: uuidGenerate,
+        first_name,
+        last_name,
+        email,
+        password,
+        birthday,
+        is_active: true,
+        url: `http://127.0.0.1:9000/users/${uuidGenerate}`
+    }
+
+    return newUser
+
+}
+
+
+// Exporting the functions across of a require 
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createNewUser
+}
+
+
+
+
--- src/todos/todos.router.js
+++ src/todos/todos.router.js
@@ -1,1 +1,19 @@
-// todos []
+// /users [GET, POST]
+// /users/:id [GET]
+
+const router =require('express').Router()
+
+// -> Importing services js
+const todoServides = require('./todos.services')
+
+
+// /users [GET, POST]
+router.get('/users')
+router.post('/users')
+
+
+// /users/:id [GET]
+router.get('/users/:id')
+
+
+
